e66a8d7bad5ec38325f1d59f08b84eaf7d543c7c
Boris Tibago <49928514+bagobob@users.noreply.github.com>
2020-12-14 11:36:59 +0100

    change house

--- src/Classe/Cart.php
+++ src/Classe/Cart.php
@@ -28,14 +28,25 @@ class Cart
             $cart[$id] = 1;
         }
 
-        $this->session->set('cart', [
-            'id' => $id,
-            'quantity' => 1
-        ]);
+        $this->session->set('cart', $cart);
     }
 
     public function get()
     {
         return$this->session->get('cart');
+    }
+
+    public function remove()
+    {
+        return$this->session->remove('cart');
+    }
+
+    public function delete($id)
+    {
+        $cart = $this->session->get('cart', []);
+
+        unset($cart[$id]);
+
+        return $this->session->set('cart',$cart);
     }
 }
--- src/Controller/CartController.php
+++ src/Controller/CartController.php
@@ -3,12 +3,21 @@
 namespace App\Controller;
 
 use App\Classe\Cart;
+use App\Entity\Product;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class CartController extends AbstractController
 {
+    private $entityManager;
+
+    public function __construct(EntityManagerInterface $entityManager)
+    {
+        $this->entityManager = $entityManager;
+    }
+
     /**
      * @Route("/mon-panier", name="cart")
      * @param Cart $cart
@@ -16,8 +25,18 @@ class CartController extends AbstractController
      */
     public function index(Cart $cart)
     {
+        $cartComplete = [];
+
+        foreach ($cart->get() as $id => $quantity)
+        {
+            $cartComplete[] = [
+                'product' => $this->entityManager->getRepository(Product::class)->findOneById($id),
+                'quantity' => $quantity
+            ];
+        }
         return $this->render('cart/index.html.twig', [
             'controller_name' => 'CartController',
+            'cart' => $cartComplete
         ]);
     }
 
@@ -45,6 +64,19 @@ class CartController extends AbstractController
 
         return $this->redirectToRoute('products');
     }
+
+    /**
+     * @Route("/cart/delete/{id}", name="delete_to_cart")
+     * @param Cart $cart
+     * @return Response
+     */
+    public function delete(Cart $cart, $id)
+    {
+        $cart->remove();
+
+        return $this->redirectToRoute('products');
+    }
+
 
 
 }
